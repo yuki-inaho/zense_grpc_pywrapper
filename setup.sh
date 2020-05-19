43de6233117722b4a35abd165a0e83ff2238dd28
yuki-inaho <yoshikawa@inaho.co>
2020-05-19 15:51:07 +0900

    Modify .gitignore

--- setup.sh
+++ setup.sh
@@ -7,6 +7,4 @@ pip install -r requirements.txt --user
 python setup.py install --user
 cd ..
 
-mkdir -p build && cd build 
-cmake .. && make -j && cd ..
 cd /app/scripts && python gen_protobuf_codes.py
